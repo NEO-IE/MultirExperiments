dffd753a78a4a10fd9eb4b14503ba1b42473ea74
Aman Madaan <madaan.amanmadaan@gmail.com>
2014-08-11 21:54:21 +0530

    Finished country marker. Returns 1 2 China /m/0d05w3 1 6 7 India /m/03rk0 1 8 9 Portugal /m/05r4w 1 given a line like Air China is progressing fast with Indians and Portugese music.

--- src/main/java/edu/washington/multir/preprocess/CountryMarker.java
+++ src/main/java/edu/washington/multir/preprocess/CountryMarker.java
@@ -5,6 +5,103 @@
  */
 package edu.washington.multir.preprocess;
 
-public class CountryMarker {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
 
+import org.apache.commons.lang3.text.WordUtils;
+
+import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.util.CoreMap;
+
+public class CountryMarker {
+	/**
+	 * We maintain 3 forms of every country : a) India, Indi and Ind which has
+	 * no space in its name. Some of the variations like US, USA were hardcoded
+	 * in the file Then we iterate over words in the sentence and see if they
+	 * match one of 3 forms of any of the countries
+	 */
+	HashMap<String, String> freeBaseMapping;
+	HashMap<String, String> completeNameMapping;
+	HashSet<String> countryList;
+
+	public CountryMarker(String countriesFile) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(new File(
+				countriesFile)));
+		String countryName = null;
+		completeNameMapping = new HashMap<String, String>();
+		freeBaseMapping = new HashMap<String, String>();
+		while ((countryName = br.readLine()) != null) {
+			String vars[] = countryName.split("\t");
+			int nl = vars[0].length();
+			// inflections of country names will never involve more than 2 last
+			// chars
+			if (nl > 2) {
+				// save the characters we have churned out
+				String suffix = vars[0].substring(nl - 2, nl);
+				vars[0] = vars[0].substring(0, nl - 2);
+				completeNameMapping.put(vars[0].toLowerCase(), suffix);
+				// need this because chinese must be stored as China, their
+				// Freebase entry
+
+			}
+
+			freeBaseMapping.put(vars[0].toLowerCase(), vars[1]);
+
+		}
+		countryList = new HashSet<String>(freeBaseMapping.keySet());
+		br.close();
+	}
+
+	/**
+	 * This method returns matchings of all the countries in the following
+	 * format StartOffset EndOffSet + 1 Country (as in freebase) freebase id 1
+	 * The last one signifies the confidence with which we find the entity TODO
+	 * : verify whether it is really the score of the ner or not? TODO : This is
+	 * (will be) painfully slow, find out another way of doing this
+	 */
+
+	String getEntityLinkString(String sentence) {
+		StringBuilder entityLinkString = new StringBuilder();
+		int wordOffSet = 0;
+		String words[] = sentence.split(" ");
+		// for (CoreLabel token : sentence.get(TokensAnnotation.class)) {
+		// String word = token.get(TextAnnotation.class);
+		for (String word : words) {
+			int wl = word.length();
+			int suffixEnd = 2;
+			while (suffixEnd < (wl -  2)) {
+				String keyWord = word.substring(0, wl - suffixEnd);
+				suffixEnd++;
+				keyWord = keyWord.toLowerCase(); // map keys are lower case
+				if (countryList.contains(keyWord)) { // we have a country match
+					String entityName = keyWord + completeNameMapping.get(keyWord);
+					String linkingString = wordOffSet + " " + (wordOffSet + 1)
+							+ " " + WordUtils.capitalize(entityName) + " "
+							+ freeBaseMapping.get(keyWord) + " 1 ";
+					entityLinkString.append(linkingString);
+					break;
+				}
+			}
+			wordOffSet++;
+		}
+		return entityLinkString.toString();
+	}
+
+	public static void main(String args[]) throws IOException {
+		/**
+		 * This is a tester for the country tagger
+		 */
+		String countriesFile = "meta/country_freebase_mapping";
+		CountryMarker cmr = new CountryMarker(countriesFile);
+		System.out
+				.println(cmr
+						.getEntityLinkString("Air China is progressing fast with Indians and Portugese music"));
+
+	}
 }
