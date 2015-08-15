f071427569054359ae6bf20b00c1ba11c5cf27d7
Aman Madaan <madaan.amanmadaan@gmail.com>
2015-08-15 20:46:38 +0530

    move the clamping code to the algo package

--- MultirFramework-master/src/main/java/edu/washington/multirframework/multiralgorithm/KeywordClamping.java
+++ MultirFramework-master/src/main/java/edu/washington/multirframework/multiralgorithm/KeywordClamping.java
@@ -2,56 +2,54 @@ package edu.washington.multirframework.multiralgorithm;
 
 import iitb.rbased.meta.KeywordData;
 
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 
-import edu.washington.multirframework.multiralgorithm.MILDocument;
-import edu.washington.multirframework.multiralgorithm.Parameters;
-import edu.washington.multirframework.multiralgorithm.Parse;
-import edu.washington.multirframework.multiralgorithm.Scorer;
+import edu.washington.multirframework.util.StemUtils;
 
-public class ConditionalInference {
+public class KeywordClamping {
 
-public static HashMap<String, Double> marginMap;
-	
+	public static HashMap<String, Double> marginMap;
+
 	static {
 		marginMap = new HashMap<String, Double>();
 	}
 
-	public static Parse infer(MILDocument doc,
-			Scorer parseScorer, Parameters params) {
+	public static Parse infer(MILDocument doc, Scorer parseScorer, Parameters params) {
 		Parse p = new Parse();
 		p.doc = doc;
 		parseScorer.setParameters(params);
 		int numMentions = doc.numMentions;
-		p.z_states = new boolean[lrg.Z.length];
-		
-		int numN = lrg.n.length;
-		for (int n_i = 0; n_i < numN; n_i++) {
+		int z[] = new int[numMentions];
+
+		for (int z_i = 0; z_i < numMentions; z_i++) {
 			HashSet<Integer> feats = new HashSet<>();
-			Number n = lrg.n[n_i];
-			List<Integer> z_s = n.zs_linked;
-			for(Integer z: z_s){
-				for(int id: lrg.features[z].ids){
-					feats.add(id);
-				}
+			for (int id : doc.features[z_i].ids) {
+				feats.add(id);
 			}
-			//Make the number true if one of the Z nodes attached expresses the relation
-			p.n_states[n_i] = hasKeyword(feats, lrg.relation);
+
+			for (Integer relId : AveragedPerceptron.relNumNameMapping.keySet()) {
+				String relName = AveragedPerceptron.relNumNameMapping.get(relId);
+				if (hasKeyword(feats, relName)) {
+					z[z_i] = relId;
+				}
+				// Make the number true if one of the Z nodes attached expresses
+				// the relation
+			}
 		}
+		p.Z = z;
+		p.Y = doc.Y;
 		return p;
 	}
-	
-	public static boolean hasKeyword(HashSet<Integer> feats, String rel){
-		List<String>  relKey = KeywordData.REL_KEYWORD_MAP.get(rel);
-		for(String key : relKey){
+
+	public static boolean hasKeyword(HashSet<Integer> feats, String rel) {
+		List<String> relKey = KeywordData.REL_KEYWORD_MAP.get(rel);
+		for (String key : relKey) {
 			String stemKey = StemUtils.getStemWord(key.toLowerCase());
-			Integer featID = LocalAveragedPerceptron.featNameNumMapping.get("key: "+stemKey);
-			if(featID != null){
-				if(feats.contains(featID)){
+			Integer featID = AveragedPerceptron.featNameNumMapping.get("key: " + stemKey);
+			if (featID != null) {
+				if (feats.contains(featID)) {
 					return true;
 				}
 			}
